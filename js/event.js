128176f495c2e4eafd024aac54ab44b8e06d2cdf
jack-ascii <baddaddy@gmail.com>
2022-01-03 22:24:17 -0700

    add quote

---
+++ js/event.js
@@ -0,0 +1,18 @@
+﻿
+ja.event = {};
+
+ja.event.prevent = function(e) {
+  if (ja.text.nul(e)) return;
+  if (!ja.text.nul(e.preventDefault)) e.preventDefault();
+  if (!ja.text.nul(e.stopImmediatePropagation)) e.stopImmediatePropagation();
+  if (!ja.text.nul(e.stopPropagation)) e.stopPropagation();
+}
+
+ja.event.lastevent = function(e) {
+  return e || window.event;
+};
+
+ja.event.lasttarget = function(e) {
+  e = ja.eventlastevent(e);
+  return (e.target || e.srcElement);
+};
--- js/text.js
+++ js/text.js
@@ -1,21 +1,23 @@
 ﻿
-function nul(inp) {
+ja.text = {};
+
+ja.text.nul = function(inp) {
   if (null === inp || undefined === inp || "undefined" === inp) { return true; }
   return false;
 }
 
-function empty(inp) {
-  if (nul(inp)) { return true; }
+ja.text.empty = function(inp) {
+  if (ja.text.nul(inp)) { return true; }
   if ("object" == typeof inp) { try { inp = inp[0].value; } catch (ex) { } }
-  return 0 === trim(inp).length;
+  return 0 === ja.text.trim(inp).length;
 }
 
-function unhtml(html) {
+ja.text.unhtml = function(html) {
   var tmp = document.createElement('DIV'); tmp.innerHTML = html;
   return tmp.textContent || tmp.innerText || '';
 }
 
-function trim(oin) {
+ja.text.trim = function(oin) {
   var sin = (null !== oin && undefined !== oin ? oin : '');
   if (null !== sin.trim && undefined !== sin.trim) {
     try { sin = sin.trim(); } catch (ex) { }
@@ -23,7 +25,7 @@ function trim(oin) {
   return sin;
 }
 
-function lower(oin) {
+ja.text.lower = function(oin) {
   var sin = (null !== oin && undefined !== oin ? oin : '');
   if (null !== sin.toLowerCase && undefined !== sin.toLowerCase) {
     try { sin = sin.toLowerCase(); } catch (ex) { }
@@ -31,29 +33,74 @@ function lower(oin) {
   return sin;
 }
 
-function indexOf(oin, fnd) {
+ja.text.indexOf = function(oin, fnd) {
   var sin = (null !== oin && undefined !== oin ? oin : ''), ret = -1;
   if (null !== sin.indexOf && undefined !== sin.indexOf) {
     try { ret = sin.indexOf(fnd); } catch (ex) { }
   }
   return ret;
 }
+ja.text.indexof = ja.text.indexOf;
 
-function replace(oin, fnd, rplc) {
+ja.text.replace = function(oin, fnd, rplc, max=9) {
   var sin = (null !== oin && undefined !== oin ? oin : '');
-  if (null !== sin.replace && undefined !== sin.replace) {
-    try { sin = sin.replace(fnd, rplc); } catch (ex) { }
-  }
+  try {
+    if (null !== sin.replace && undefined !== sin.replace) {
+      sin = sin.replace(fnd, rplc);
+    }
+    else {
+      var n = 0;
+      while (-1 < ja.text.indexOf(sin, fnd) && n++ < max)
+        sin = sin.replace(fnd, rplc);
+    }
+  } catch (ex) { ja.log.warn('ja.text.replace()', ex); }
   return sin;
 }
 
-Number.prototype.trunc = function(digits) {
-    var re = new RegExp("(\\d+\\.\\d{" + digits + "})(\\d)"),
-        m = this.toString().match(re);
-    return m ? parseFloat(m[1]) : this.valueOf();
+ja.text.hash = function(target, quiet, duration) {
+  duration = duration || 15*1000;
+  if (!ja.text.nul(target)) {
+    if (quiet) ja.cache.set('hash', target, 15*1000);
+    else document.location.hash = target;
+  }
+  var hsh = document.location.hash.split('&')[0];
+  if (ja.text.empty(hsh)) hsh = ja.cache.get('hash');
+
+  if (ja.text.empty(hsh)) return '';
+  if (0 > ja.text.indexof(hsh, '#')) hsh = '#'+ hsh;
+
+  return hsh;
+}
+
+ja.text.truncnumber = function(digits) {
+  var re = new RegExp("(\\d+\\.\\d{" + digits + "})(\\d)"),
+      m = this.toString().match(re);
+  return m ? parseFloat(m[1]) : this.valueOf();
+}
+
+ja.text.shuffle = function(array) {
+  let currentIndex = array.length,  randomIndex;
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  return array;
+}
+
+ja.text.decode = function(input) {
+  var doc = new DOMParser().parseFromString(input, "text/html");
+  return doc.documentElement.textContent;
 };
 
-function hash(target) {
-  if (!nul(target)) document.location.hash = target;
-  return document.location.hash.split('&')[0];
+ja.text.clearto = function(timeout) {
+  if (!ja.text.nul(timeout)) clearTimeout(timeout);
+  timeout = null;
+  return null;
 }
+
+Number.prototype.trunc = function(digits) {
+  return ja.text.truncnumber(digits);
+};
