8d18a1931e638a202f839282b117b9d54eb6cfc6
jack-ascii <baddaddy@gmail.com>
2022-01-03 22:24:17 -0700

    add quote

---
+++ js/error.js
@@ -0,0 +1,42 @@
+﻿
+ja.error = ja.error || {};
+
+ja.error.handle = function(msg, url, line, col, err) {
+	var cleaned = ja.error.clean(msg, '^uncaught( |:),^referenceerror( |:),^typeerror( |:),^syntaxerror( |:),not defined=undefined');
+  var stack = (null !== err && undefined !== err && null !== err.stack && undefined !== err.stack ? err.stack : '');
+
+  var lmsg = ja.text.lower(ja.text.trim(msg));
+  var lerr = ja.text.lower(ja.text.trim(err ? err.toString() : ''));
+  var strings = [lmsg, lerr];
+  for (var s in strings) {
+    var rx = ja.text.trim(strings[s]).match(/^(((uncaught |unhandled )?script error)|syntaxisfout)[.]?$/gi);
+    if (rx && 0 < rx.length) return true;
+  }
+
+  ja.log.warn('ja.error.handle()', cleaned, stack);
+
+	if ('1' === ja.qs2('dbg') && !ja.text.nul(ja.ui.toast)) { ja.ui.toast(cleaned); }
+  return true;
+}
+
+ja.error.clean = function(ein, scrub) {
+  //console.log('ja.error.clean()', 'ein=', ein, scrub);
+  var eout = ((!ja.text.empty(ein) ? ein : '') + '');
+  var sa = scrub.split(',');
+  for (var s in sa) {
+    var sc = sa[s], sp = sc.split('=');
+    if (!ja.text.empty(sc) && !ja.text.empty(sp[0])) {
+      var rx = new RegExp(sp[0], "gi");
+      eout = eout.replace(rx, 1 < sp.length ? sp[1] : '');
+    }
+  }
+  //console.log('ja.error.clean()', 'eout=', eout);
+  return ja.text.trim(eout);
+}
+
+if ('0'!==ja.qs2('err'))
+  window.onerror =
+    function(msg,url,line,col,err) {
+      return ja.error.handle(msg,url,line,col,err);
+    };
+
--- js/toast.js
+++ js/toast.js
@@ -1,15 +1,30 @@
 ﻿
-/* common/toasts.js */
-function toast(msg,exp) {
+ja.ui = ja.ui || {};
+
+ja.ui.toast = function(msg,exp) {
+  //ja.log.dbg('ja.ui.toast()', 'msg=', msg, 'exp=', exp);
+
 	var toasts = $('#toasts'), unq = new Date().getTime();
-	if (0===toasts.length) { toasts = $('.toasts'); }
-	if (0===toasts.length) { 
+
+	if (!toasts.length) toasts = $('.toasts');
+
+	if (!toasts.length) {
 		var par = $('main');
 		if (0===par.length) { par = $('body'); }
 		toasts = $('<div id="toasts" class="toasts"></div>').prependTo(par);
 	}
-	var toast = $('<p id="' + unq + '" class="show toast">' + msg + '</p>').appendTo(toasts);
-	$(toast).click(function () { $(this).toggleClass('show hide').remove(); });
-	if (nul(exp)) { exp = 5; }
-	if (0 < exp) { setTimeout("$('#" + unq + "').toggleClass('show hide').remove();", exp*1000); }	
+
+	var toast =
+		$('<p id="' + unq + '" class="show toast">' + msg + '</p>')
+			.appendTo(toasts);
+
+	$(toast).click(function () {
+		$(this).toggleClass('show hide').remove();
+	});
+
+	if (ja.text.nul(exp))
+		exp = 5;
+
+	if (0 < exp)
+		setTimeout("$('#" + unq + "').toggleClass('show hide').remove();", exp*1000);
 }
